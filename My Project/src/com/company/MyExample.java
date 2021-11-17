2fb7f53c6992e9e56f2eb6db2887e7dd1da92ec5
osamamalik234 <osamamalik9171@gmail.com>
2021-11-17 00:44:19 -0800

    Committed

---
+++ My Project/src/com/company/MyExample.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MyExample {
+    public static void main(String[] args) {
+       int a = 3;
+       int b = 4;
+       int sum = a + b;
+       int sub = a-b;
+       int mul = a * b;
+       System.out.println(sum);
+       System.out.println(sub);
+       System.out.println(mul);
+    }
+}
